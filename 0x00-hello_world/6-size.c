ccfb0635335caaedeb7d0928306fbe2094004e32
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-10 13:48:10 -0800

    Creation of the file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-one
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)");
+
+return (0);
+
+}
