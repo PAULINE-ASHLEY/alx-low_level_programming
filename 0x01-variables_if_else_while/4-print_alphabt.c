17a4d226a3587898cd28cf6855d57aeb689a4a08
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-11 16:48:21 -0800

    Creation of the file 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,15 @@
 int main(void)
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
+	
         if (ch == 'q' || ch == 'e')
-	{
+	
 	continue;
-        }
+        
         putchar(ch);
-	}
-	putchar('\n');
+	
+putchar('\n');
 	return (0);
 }
