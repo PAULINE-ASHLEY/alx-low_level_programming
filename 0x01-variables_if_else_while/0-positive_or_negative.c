b11e829ba5566cb622fce8f5078204d304dea3ba
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-11 17:11:56 -0800

    Creation of the file 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,5 +7,22 @@
  */
 int main(void)
 {
+	int ch;
+
+	srand(time(0));
+	ch = rand() - RAND_MAX / 2;
+	/* your code goes here */
+	if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+	else if (n > 0)
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else 
+	{
+		printf("%d is %s\n", n, "zero");
+	}
 	return (0);
 }
