c6673c7f1e72e49df0c1b611e91e672aa8955a6c
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-13 06:42:34 -0700

    Creation of the file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,17 @@
  */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n");
-printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 4 byte(s)\n");
-printf("Size of a long long int: 8 byte(s)\n");
-printf("Size of a float: 4 byte(s)\n");
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(e));
 
 return (0);
 
