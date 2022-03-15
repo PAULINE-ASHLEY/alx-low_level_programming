88c956f46bef5a67869543639cf84faea3ba455e
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-15 15:46:06 -0700

    file 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,10 @@
 
 /**
  * is_alpha - checks for alphabetic character
- * c - the character to be checked
+ * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-int is_alpha(int c)
+int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
