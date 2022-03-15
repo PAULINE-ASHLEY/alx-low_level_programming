0f58c0716d4d42e7b8153f1b3f46de1eefb6f8a2
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-15 12:08:20 -0700

    file 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - End point
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+
+	int i;
+
+	i = 0;
+	
+	while (i < 10)
+	{
+		char p;
+
+		p = 'a';
+
+		while (p <= 'z')
+		{
+			_putchar(p);
+			p++;
+		}
+	}
+
+	_putchar('\n');
+	i++;
+}
