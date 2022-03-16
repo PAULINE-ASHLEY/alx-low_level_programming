62f565804dec42ced3ecc675940ee32d152a29b3
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-15 17:18:01 -0700

    file update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - End point
- *
- * Return: Always 0
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
@@ -12,7 +11,6 @@ void print_alphabet_x10(void)
 	int i;
 
 	i = 0;
-	
 	while (i < 10)
 	{
 		p = 'a';
