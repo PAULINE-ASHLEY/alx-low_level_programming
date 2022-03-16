b21d3e6e9c12e48a1c338094f8bf9337e8b7fa7a
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-15 17:57:05 -0700

    file update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,10 +9,10 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 0; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-			if ( j == 0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
