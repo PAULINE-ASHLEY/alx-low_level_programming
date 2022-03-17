a7a1e086d8b74c492d5fdd7425143cf4b177ff05
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-17 09:20:30 -0700

    upload files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - End point
+ *
+ * Retrun: Always 0
+ */
+int _isupper(int c)
+{
+	return(c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - End point
+ *
+ * Return : Always 0 (success)
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
