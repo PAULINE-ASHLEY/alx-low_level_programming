d8855558d073025db59b88835c9461386ff736e2
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-17 09:28:59 -0700

    file upload

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - End point
- *
- * Retrun: Always 0
+ * _isupper - checks fora digit through 0 to 9
+ * @c: int to be checked
+ * Return: 1 if c is a digit, otherwise 0
  */
 int _isupper(int c)
 {
-	return(c >= 'A' && c <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
