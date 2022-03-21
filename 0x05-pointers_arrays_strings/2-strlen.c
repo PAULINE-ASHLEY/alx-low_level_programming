bf55541f35760cf35ba5626efbd78da2b16a0dea
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-21 15:45:06 -0700

    file 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,11 +9,11 @@
  */
 int _strlen(char *s)
 {
-	char s[16] = "My first strlen!";
+	char *s;
 	int i;
 
-	for (i = 0; s[i] != '\0'; ++i);
+	*s = "My first strlen!";
+	i = _strlen(*s);
 	_putchar(i);
-
 	return (0);
 }
