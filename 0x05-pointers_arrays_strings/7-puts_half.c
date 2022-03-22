cb72281d8b73bb1df0c6b81081f13359e049bc7f
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2022-03-21 17:20:28 -0700

    file 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 void puts_half(char *str)
 {
 	int len, n, i;
-	
+
 	while (str[len] != '\0')
 	{
 		len++;
@@ -16,7 +16,7 @@ void puts_half(char *str)
 
 	if (len % 2 == 0)
 	{
-		for ( i = len / 2; str[i] != '\0'; i++)
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
